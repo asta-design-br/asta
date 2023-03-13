2f1450cc6658b725eedefb3f733f9a3be16c9b31
Daniela Rios <danirioscampelo@gmail.com>
2023-03-13 19:13:12 -0300

    new user columns attributes added to new and edit registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name username profile document]
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name username profile document]
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name username profile document])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name username profile document])
   end
 end
