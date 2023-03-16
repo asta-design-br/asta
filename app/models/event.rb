2decb72b01f9b3bfa73bcf94640aa44e3cb1edd8
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2023-03-16 18:06:29 -0300

    Started adding associations and validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  has_many :users, :active_storage_attachments
+  validates :title, length: { maximum: 50 }, presence: true
+  validates :description, length: { minimum: 50 }, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
