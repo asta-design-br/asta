41b343eed59da412976e55bb7c7662eb6dcd6da6
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2023-03-20 20:06:13 -0300

    Event model update

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :users
   has_one :address, as: :addressable
   has_one :phone, as: :phonable_id
+  has_many_attached :visual_references
   validates :title, length: { maximum: 50 }, presence: true
   validates :description, length: { minimum: 50 }, presence: true
   validates :start_time, presence: true
