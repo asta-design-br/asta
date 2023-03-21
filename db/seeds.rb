5146ab139784e0d903ad4139f5e1be667c72f43b
Daniela Rios <danirioscampelo@gmail.com>
2023-03-21 18:02:58 -0300

    user created in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
 puts "destroying every User"
 User.destroy_all
 
@@ -12,9 +8,13 @@ puts "destroying every Role"
 Role.destroy_all
 
 puts "creating user..."
-User.create(
+User.create!(
   email: 'dev@test.com',
-  password: '123456'
+  password: '123456',
+  full_name: "Daniela Rios",
+  username: "dani_rioss",
+  profile: "Sou designer gráfica. Trabalho conforme a necessidade do cliente para obter os melhores resultados, com uma visão jovem, atualizada e sempre aberto às novidades do mercado.",
+  document: "023729341-20"
 )
 
 puts "creating roles..."
@@ -23,3 +23,5 @@ puts "creating roles..."
     name: role
   )
 end
+
+puts "Fim"
