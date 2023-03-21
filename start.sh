55565afca73d671dca82f2c221ce3b6a38d0fae3
João Esmanhoto <joao.esmanhoto@olgari.com.br>
2023-03-21 17:26:55 -0300

    remove volumes to restart app after gem and packeges updates

--- start.sh
+++ start.sh
@@ -4,5 +4,12 @@ set -v
 echo "removing all containers..."
 docker ps -aq | xargs docker stop | xargs docker rm
 
+echo "removing old web image..."
+docker rmi asta-web
+
+echo "removing caches packages and gems"
+docker volume rm asta_gem_cache
+docker volume rm asta_node_modules
+
 bundle install
 docker-compose up
