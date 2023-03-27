fb362b1c4249cb6436f5c3e250d6faaff5afd06c
Daniela Rios <danirioscampelo@gmail.com>
2023-03-27 20:26:59 -0300

    it was missing a little thing

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,14 +8,14 @@ class Product < ApplicationRecord
   validates :required_time, numericality: { only_integer: true, greater_than: 0 }
   # validates :category, inclusion: { in: %w[flyer poster video], message: '%<value> is not a valid role.' }
 
-  validate :check_presence_of_pixels_and_mm
-
-  def check_presence_of_mm
-    mm_height.positive? && mm_width.positive? if pixels_height.nil? && pixels_width.nil?
-  end
+  validate :check_presence_of_pixels, :check_presence_of_mm, :check_presence_of_milliseconds_length
 
   def check_presence_of_pixels
     pixels_height.positive? && pixels_width.positive? if mm_height.nil? && mm_width.nil?
+  end
+
+  def check_presence_of_mm
+    mm_height.positive? && mm_width.positive? if pixels_height.nil? && pixels_width.nil?
   end
 
   def check_presence_of_milliseconds_length
