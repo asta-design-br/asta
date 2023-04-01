10a6eea4e1ade3bf5327ff61e660fdb5d097fb3c
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2023-04-01 10:25:25 -0300

    fix tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,12 +7,10 @@ class Event < ApplicationRecord
   has_many :addresses, as: :asta_addressable
   has_many :phones, as: :phonable
   has_many_attached :visual_references
-  
+
   validates :title, :description, :start_time, :end_time, presence: true
-  # rubocop:disable Style/StringLiterals, Style/FormatStringToken
-  validates :title, length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
-  validates :description, length: { minimum: 50, too_short: "is too short (minimum is %{count} characters)" }
-  # rubocop:enable Style/StringLiterals, Style/FormatStringToken
+  validates :title, length: { maximum: 50, too_long: '%{count} characters is the maximum allowed' }
+  validates :description, length: { minimum: 50, too_short: 'is too short (minimum is %{count} characters)' }
   validate :start_time_cannot_be_in_the_past, :end_time_cannot_be_before_start_time
 
   private
