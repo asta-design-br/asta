e4611a903c8cff8f0ccb59bbf18d71042c44b6ac
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2023-04-03 15:18:37 -0300

    Improved code following Juca's recommendations and deleted Bid's files in models and factories

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,12 +2,14 @@ class Auction < ApplicationRecord
   belongs_to :product
   has_many :bids, dependent: :destroy
   validates :deadline, :product, presence: true
-  validate :deadline_after_now_before_start_time
+  validate :deadline_custom_validate
 
   private
 
-  def deadline_after_now_before_start_time
-    return if deadline.present? && product.present? && deadline > DateTime.now && deadline < product.event.start_time
+  def deadline_custom_validate
+    is_before_event_start = product.present? && deadline < product.event.start_time
+    is_after_now = deadline > DateTime.now
+    return if deadline.present? && is_after_now && is_before_event_start
 
     errors.add(:deadline, 'must be after now and before event start time')
   end
--- spec/factories/bids.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :bid do
-    user { nil }
-    auction { nil }
-  end
-end
