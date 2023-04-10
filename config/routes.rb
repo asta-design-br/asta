68a04b2227515d5627446c97b6dd6aa6b5aa300d
Daniela Rios <danirioscampelo@gmail.com>
2023-04-10 10:04:26 -0300

    rubocop fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  root to: "pages#home"
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
