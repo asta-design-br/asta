54cd020889a208c617848f5d0797140cdba67d90
Araceli Martinez <araceli_martinez94@hotmail.com>
2023-06-05 18:01:06 -0300

    add sign up style and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  # Define custom routes for choosing user role and signing up as a producer or designer
+  devise_scope :user do
+    get '/sign_up_choice', to: 'registrations#sign_up_choice', as: :sign_up_choice
+  end
+
   root to: 'pages#home'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  
   # Defines the root path route ("/")
   # root "articles#index"
 end
